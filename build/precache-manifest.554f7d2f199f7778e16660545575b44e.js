b27352f95bd603b3c55b2dcb8083cd8301b4fcaf
wchan <Because1215>
2020-02-28 20:30:21 -0500

    Added procfile

---
+++ build/precache-manifest.554f7d2f199f7778e16660545575b44e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6ab07507e32d25359a2173e4f53122",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59254fc659dbe536edce",
+    "url": "/static/css/main.060398b0.chunk.css"
+  },
+  {
+    "revision": "676369ebca72f4ffbf5d",
+    "url": "/static/js/2.306698ee.chunk.js"
+  },
+  {
+    "revision": "59254fc659dbe536edce",
+    "url": "/static/js/main.313e2aca.chunk.js"
+  },
+  {
+    "revision": "8339aa2ba8bf82f99f91",
+    "url": "/static/js/runtime-main.27512ada.js"
+  },
+  {
+    "revision": "fe611d57aaf8b3ec0eabbd09602ea51a",
+    "url": "/static/media/halloween.fe611d57.png"
+  }
+]);
