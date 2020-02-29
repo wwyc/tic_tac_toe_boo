db31fbe7395874c851cf640d25cc12ad6b941427
wchan <Because1215>
2020-02-28 21:05:36 -0500

    Updates

---
+++ precache-manifest.b8b6c9fb864beb5faa97bb551e4a02e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10aa0d7792f6c967992b4f3ead0e4a9b",
+    "url": "/tic_tac_toe_boo/index.html"
+  },
+  {
+    "revision": "e3325d5b3510cd4cedbf",
+    "url": "/tic_tac_toe_boo/static/css/main.237c624d.chunk.css"
+  },
+  {
+    "revision": "676369ebca72f4ffbf5d",
+    "url": "/tic_tac_toe_boo/static/js/2.306698ee.chunk.js"
+  },
+  {
+    "revision": "e3325d5b3510cd4cedbf",
+    "url": "/tic_tac_toe_boo/static/js/main.9b413980.chunk.js"
+  },
+  {
+    "revision": "ca94468872def479f4c6",
+    "url": "/tic_tac_toe_boo/static/js/runtime-main.117daf41.js"
+  },
+  {
+    "revision": "fe611d57aaf8b3ec0eabbd09602ea51a",
+    "url": "/tic_tac_toe_boo/static/media/halloween.fe611d57.png"
+  }
+]);
